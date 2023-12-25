f799a39abb9895129c0fddc0306780a8f3e709b8
anubhav bisht <anubhavbisht98@gmail.com>
2023-12-25 21:22:11 +0530

    learnt about parcel

--- chapter2/dist/index.6bd02f5a.js
+++
@@ -1,5 +0,0 @@
-const reactHeading = React.createElement("h3", {}, "Hello World!(REACT code)");
-const reactRoot = ReactDOM.createRoot(document.getElementById("react-root"));
-reactRoot.render(reactHeading);
-
-//# sourceMappingURL=index.6bd02f5a.js.map
